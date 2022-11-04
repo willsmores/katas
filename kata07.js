0ed725cdd962f8b3065f552393ed9745a94e6e44
Steve Willsmore <scwillsmore@gmail.com>
2022-11-04 16:57:15 -0400

    Initial commit

---
+++ kata07.js
@@ -0,0 +1,39 @@
+const checkAir = function (samples, threshold) {
+  
+  let cleanCount = 0;
+  let dirtyCount = 0;
+  
+  for (const sample of samples) {
+    if (sample === 'clean') {
+      cleanCount++;
+    } else if (sample === 'dirty') {
+      dirtyCount++;
+    } else {
+      return 'Please enter a valid sample condition.'
+    }
+  };
+  
+  const dirtyProportion = dirtyCount / cleanCount;
+  
+  if (threshold < dirtyProportion) {
+    return 'Polluted';
+  } else {
+    return 'Clean'
+  };
+  
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
